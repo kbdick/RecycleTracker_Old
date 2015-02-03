10d49c4bcfdbad4a9f2f9d8473fd2783d435a660
Kevin Dick <kbdick@gmail.com>
2015-02-03 02:15:56 +0000

    Change div name

--- js/recycletracker.js
+++ js/recycletracker.js
@@ -44,7 +44,7 @@ jQuery(document).ready(function($) {
                           labels: "undefined",
                           m: [80, 100, 120, 100],
                           w: 800, h: 400,
-                          div: "#pennies",
+                          div: "#martTest",
                           yaxis: "Lbs Recycled",
                           hiColor: "#E4EB29"
                         }

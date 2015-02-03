d02f06988583aa0406bc866787468dcb40fbb58d
Kevin Dick <kbdick@gmail.com>
2015-02-03 01:42:57 +0000

    Changes to chart ui

---
+++ js/scratch.js
@@ -0,0 +1,19 @@
+  document.addEventListener('DOMContentLoaded', function() {
+   	var URL = "1o2CslDz13ipgg7U3txgvRNMek38ul4S8UWJL_lravMQ";
+  		Tabletop.init( { key: URL, callback: showInfo, simpleSheet: true } )
+  	});
+
+    function showInfo(data) {
+      var cali = Sheetsee.getOccurance(data, "weight")
+      var colors = ['#5cb85c', '#acacac'];
+      var caliData = Sheetsee.makeColorArrayOfObject(cali, colors)
+      var lineOptions = { units: "Weight",
+                          labels: "weight",
+                          m: [10, 10, 10, 10],
+                          w: 800, h: 400,
+                          div: "#pennies",
+                          yaxis: "Weight",
+                          hiColor: "#E4EB29"
+                        };
+      Sheetsee.d3LineChart(caliData, lineOptions);
+    }
